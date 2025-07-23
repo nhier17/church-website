e33ebc5ab245edeccdd14dc7baf031cf30e8fa01
Abraham <abrahamnhier97@gmail.com>
2025-07-23 21:16:36 +0300

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "Nairobi Baptist Church",
+  description: "A family that prays together, stands together",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 max-w-7xl mx-auto">
+      <h1 className="text-4xl font-bold">Nairobi Baptist Church</h1>
+    </div>
+  );
+}
